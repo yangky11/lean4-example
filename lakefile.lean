63265b11f77bc1db9eb2282c744233e32d46b518
Kaiyu Yang <kaiyuy@cs.princeton.edu>
2023-05-03 21:38:37 -0400

    add dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -9,6 +9,10 @@ lean_lib «Lean4Example» {
   -- add library configuration options here
 }
 
+require std from git "https://github.com/leanprover/std4" @ "main"
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 @[default_target]
 lean_exe «lean4-example» {
   root := `Main
