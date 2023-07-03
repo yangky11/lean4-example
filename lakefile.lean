7d711f6da4584ecb7d4f057715e1f72ba175c910
Kaiyu Yang <kaiyuy@caltech.edu>
2023-07-03 15:37:12 -0700

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -9,9 +9,6 @@ lean_lib «Lean4Example» {
   -- add library configuration options here
 }
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
-
 @[default_target]
 lean_exe «lean4-example» {
   root := `Main
