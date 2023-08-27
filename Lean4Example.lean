1ac33f05939fdfa0a0e50a2fa65125a6474c07c0
Kaiyu Yang <kaiyuy@caltech.edu>
2023-08-27 11:29:20 -0700

    add another theorem

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -3,5 +3,8 @@ open Nat (add_assoc add_comm)
 def hello := "world"
 
 theorem hello_world (a b c : Nat) 
-  : a + b + c = a + c + b := by 
+  : a + b + c = a + c + b := by
   rw [add_assoc, add_comm b, ←add_assoc]
+
+theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
+
