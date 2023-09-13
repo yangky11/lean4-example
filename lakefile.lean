0aef8d7da2ee80af7ed60699e467a1ae70719414
Kaiyu Yang <kaiyuy@cs.princeton.edu>
2023-09-13 08:22:22 -0500

    add noLeanInferCloudRelease

--- lakefile.lean
+++ lakefile.lean
@@ -15,4 +15,8 @@ lean_exe «lean4-example» {
 }
 
 require aesop from git "https://github.com/JLimperg/aesop"
-require LeanInfer from git "https://github.com/lean-dojo/LeanInfer.git"@"v0.0.3"
+require LeanInfer from git "https://github.com/lean-dojo/LeanInfer.git"@"v0.0.3" with
+  if let some noLeanInferCloudRelease := get_config? noLeanInferCloudRelese then
+    NameMap.empty.insert `noCloudRelease noLeanInferCloudRelease
+  else
+    NameMap.empty
