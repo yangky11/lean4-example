5117c1d326f0505bef137c7c99099f3f780624b9
Kaiyu Yang <kaiyuy@caltech.edu>
2023-12-08 19:09:53 -0800

    minor update

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -1,11 +1,9 @@
 import Mathlib.Data.Set.Basic
 import LeanCopilot
 
-#configure_llm_aesop
-
 
 theorem add_abc : ∀ a b c : ℕ, a + b + c = a + c + b := by
-  intros a b c
+  intro a b c
   simp [Nat.add_assoc, Nat.add_comm b]
 
 
