41f6a6aed00cfb71326dd9d941f7427ee3ae0cb7
Kaiyu Yang <kaiyuy@caltech.edu>
2023-12-11 12:10:06 -0600

    bump

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 package «lean4-example» {
   moreLinkArgs := #[
-    "-L./.lake/packages/LeanInfer/.lake/build/lib",
+    "-L./.lake/packages/LeanCopilot/.lake/build/lib",
     "-lctranslate2"
   ]
 }
@@ -18,4 +18,4 @@ lean_lib Gcd {
 }
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
-require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "main"
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.0.0"
