4e853dae4ca94154eb7b688d5292877dce05f197
Kaiyu Yang <kaiyuy@meta.com>
2025-01-04 15:35:24 +0000

    bump to v4.15.0

--- lakefile.lean
+++ lakefile.lean
@@ -12,4 +12,4 @@ package «lean4-example» {
 lean_lib «Lean4Example» {
 }
 
-require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v4.14.0"
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v4.15.0"
